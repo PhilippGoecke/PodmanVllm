b4bd5d3dddfdb7ed1a093ba146c9223679976f25
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-24 19:57:32 +0200

    Add environment variable for VLLM API key

--- podman-run-client.bash
+++ podman-run-client.bash
@@ -1,2 +1,2 @@
 podman build --no-cache --rm --file Containerfile.Client --tag vllm:client .
-podman run --interactive --tty vllm:client
+podman run --interactive --tty --env VLLM_API_KEY="API_KEY" vllm:client
